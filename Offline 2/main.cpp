73ee07dcb3773d4d7c4eb83392e13a98e063f9f5
Latif siddiq sunny <1505069.mls@ugrad.cse.buet.ac.bd>
2018-11-21 18:35:59 +0600

    Creating node class for offline 2

--- Offline 2/main.cpp
+++ Offline 2/main.cpp
@@ -3,6 +3,80 @@ using namespace std;
 
 int n;
 
+class Node{
+    vector<vector<int> > board;
+    Node *parent;
+    int priority=0;
+
+public:
+    Node(){
+        parent=NULL;
+        priority=0;
+    }
+    Node(vector<int>v){
+        parent=NULL;
+        vector<int>x(n);
+        for(int i=0;i<n;i++){
+            x.clear();
+
+            for(int j=0;j<n;j++){
+                x.push_back(v[i*n+j]);
+            }
+            board.push_back(x);
+
+        }
+        priority=0;
+    }
+    Node(const Node &node){
+        this->board=node.board;
+        this->parent=new Node();
+        priority=node.priority;
+        *parent=*node.parent;
+
+    }
+
+    Node operator =(const Node& node) {
+        this->board=node.board;
+        this->parent=new Node();
+        priority=node.priority;
+        *parent=*node.parent;
+    }
+    void setpriority(int value){
+        priority= value+h();
+    }
+    int getpriority(){
+        return priority;
+    }
+    int h(){
+
+    }
+    bool operator <(const Node& node) {
+        if(priority<=node.priority) return true;
+        else return false;
+    }
+
+
+    friend ostream& operator<<(ostream& os, const Node& node);
+
+    ~Node(){
+        delete  parent;
+        parent=NULL;
+    }
+};
+ostream& operator<<(ostream& os, const Node& node){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(node.board[i][j]==-1){
+                cout<<"  ";
+                continue;
+            }
+            cout<<node.board[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+
 int stringtoint(string s){
     int  num=0;
     int sz=s.size();
@@ -21,11 +95,23 @@ int stringtoint(string s){
 int inversioncount(vector<int>v){
     int co=0;
     for(int i=0;i<v.size();i++){
+        if(v[i]==-1) continue;
         for(int j=i+1;j<v.size();j++){
+            if(v[j]==-1) continue;
             if(v[i]>v[j]) co++;
         }
     }
     return co;
+}
+Node GoalGenerator(){
+
+    vector<int>v;
+    for(int i=1;i<n*n;i++){
+        v.push_back(i);
+    }
+    v.push_back(-1);
+    Node Goal(v);
+    return Goal;
 }
 int main() {
     cout<<"Enter board size : ";
@@ -41,13 +127,14 @@ int main() {
         while ( getline( iss, x, ' ' ) ) {
             if(x.size()==0) {
                 rem=i;
+                v.push_back(-1);
                 continue;
             }
             int y=stringtoint(x);
             v.push_back(y);
         }
     }
-  /*  for(int i=0;i<v.size();i++){
+   /*for(int i=0;i<v.size();i++){
         cout<<v[i]<<" ";
     }
     cout<<endl;
@@ -60,6 +147,9 @@ int main() {
             cout<<"No solution for this board"<<endl;
             return 0;
         }
+        Node start(v);
+        Node Goal=GoalGenerator();
+        cout<<Goal;
 
     }else{
         if((rem+inversion)%2==0){
@@ -68,5 +158,6 @@ int main() {
         }
 
     }
+
 
 }
